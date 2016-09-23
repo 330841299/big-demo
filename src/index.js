a7ed66643b37cbca5be6908824f821b160b75ec4
330841299 <330841299@qq.com>
2016-09-23 09:52:16 +0800

    sdfw

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {render} from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Routers from './routers';
-import './main.css';
-
-
-render(< Routers />,document.getElementById('root'));
